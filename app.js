056c5734213a430bb43644895311707945fc591b
abdulazis530 <abdlazis530@gmail.com>
2021-04-05 23:06:04 +0700

    refactoring code

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const fileUpload = require("express-fileupload");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +15,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- faceApiService.js
+++ faceApiService.js
@@ -3,11 +3,9 @@ const fs = require('fs').promises
 const tf = require("@tensorflow/tfjs-node");
 const faceapi = require("@vladmandic/face-api/dist/face-api.node.js");
 const modelPathRoot = "./models";
-// const REFERENCE_IMAGE = '/images/rbt1.jpg'
-// const referenceImage = await canvas.loadImage(REFERENCE_IMAGE)
-// console.log(referenceImage);
+
 let optionsSSDMobileNet;
-async function image(file) {
+async function bufferToTensor(file) {
   const decoded = tf.node.decodeImage(file);
   const casted = decoded.toFloat();
   const result = casted.expandDims(0);
@@ -15,71 +13,53 @@ async function image(file) {
   casted.dispose();
   return result;
 }
-
-// async function detect(tensor) {
-//     try {
-//         const result = await faceapi.detectAllFaces(tensor, optionsSSDMobileNet)
-//         return result;
-//     } catch (err) {
-//         log.error('Caught error', err.message);
-//         return [];
-//     }
-// }
-
-async function main(file) {
+async function detect(file) {
   try {
-    console.log("FaceAPI single-process test");
     await faceapi.tf.setBackend("tensorflow");
     await faceapi.tf.enableProdMode();
     await faceapi.tf.ENV.set("DEBUG", false);
     await faceapi.tf.ready();
-  
+
     const modelPath = path.join(__dirname, modelPathRoot);
-  
+
     await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
     await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath);
     await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath);
-  
+
     optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({
       minConfidence: 0.5,
     });
-  
-  
+
+    //take data image from public directory
     let refImageDir = path.join(__dirname, `./public/images/rbt1.jpg`)
     const data = await fs.readFile(refImageDir, { encoding: 'base64' })
-    const refTensor = await image(Buffer.from(data, 'base64'))
-    const queryTensor = await image(file)
+
+    //convert base64 to buffer
+    const bufferData = Buffer.from(data, 'base64')
+    //variable file is already buffer, convert both buffer data into tensor 
+    const refTensor = await bufferToTensor(bufferData)
+    const queryTensor = await bufferToTensor(file)
+
     const refResult = await faceapi
       .detectAllFaces(refTensor)
       .withFaceLandmarks()
       .withFaceDescriptors()
-   
-    if (!refResult.length) {
-      return
-    }
+    if (!refResult.length) return
+
     // create FaceMatcher with automatically assigned labels
     // from the detection results for the reference image
     const faceMatcher = new faceapi.FaceMatcher(refResult)
+
     const queryResult = await faceapi
       .detectSingleFace(queryTensor)
       .withFaceLandmarks()
       .withFaceDescriptor()
-    if (queryResult) {
-      const bestMatch = faceMatcher.findBestMatch(queryResult.descriptor)
-      console.log(bestMatch);
-      console.log(bestMatch.toString())
-    }
+
+    //compare similarity between refResult and faceMatcher
+    if (queryResult) return faceMatcher.findBestMatch(queryResult.descriptor).toString()
+
   } catch (error) {
     console.log(error);
   }
- 
-
-
-
-  // const result = await detect(tensor);
-  // tensor.dispose();
-  // return result;
 }
-module.exports = {
-  detect: main,
-};
+exports.detect=detect  
